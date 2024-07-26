73f507ca874a449e4003873da3364547dd297266
János Illés <janos.illes@formlabs.com>
2024-07-26 13:18:12 +0200

    use async loader and awaits to avoid loading racecondition
    lettes were loaded in the wrong order sometimes otherwise

--- src/scene.ts
+++ src/scene.ts
@@ -2,44 +2,32 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { STLLoader } from 'three/examples/jsm/Addons.js';
 
-function setup3DCanvas(canvasContainer: HTMLElement) {
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new STLLoader()
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
-    const controls = new OrbitControls(camera, renderer.domElement);
-
+async function setup3DCanvas(canvasContainer: HTMLElement) {
     renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
     canvasContainer.appendChild(renderer.domElement);
     const material = new THREE.MeshMatcapMaterial()
-    const loader = new STLLoader()
 
     let Xpos = 0;
 
-    function loadGlyph(glyph: string) {
-        loader.load(
-            `font/${glyph}.stl`,
-            function (geometry) {
-                const mesh = new THREE.Mesh(geometry, material)
-                scene.add(mesh);
-                mesh.position.set(Xpos, 0, 0);
-                Xpos += 0.29;
-                mesh.rotateY(0.7);
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log(error)
-            }
-        )
+    async function loadGlyph(glyph: string) {
+        const geometry = await loader.loadAsync(`font/${ glyph }.stl`);
+        const mesh = new THREE.Mesh(geometry, material)
+        scene.add(mesh);
+        mesh.position.set(Xpos, 0, 0);
+        Xpos += 0.29;
+        mesh.rotateY(0.7);
     }
 
-    loadGlyph("FL");
-    loadGlyph("OA");
-    loadGlyph("RB");
-    loadGlyph("MS");
-
+    await loadGlyph("FL");
+    await loadGlyph("OA");
+    await loadGlyph("RB");
+    await loadGlyph("MS");
 
     camera.position.z = 2.5;
     controls.update();
@@ -65,8 +53,13 @@ function setup3DCanvas(canvasContainer: HTMLElement) {
     }
 }
 
-function update3DText(letterPairs: string[]) {
-    console.log(letterPairs);
+function update3DText(glyphs: string[]) {
+    while(scene.children.length > 0){
+        scene.remove(scene.children[0]);
+    }
+    glyphs.forEach(glyph => {
+
+    });
 }
 
 export { setup3DCanvas, update3DText }
