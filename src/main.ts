50f90dccb25ffa836d2aabcd4d50ad02231b3d03
János Illés <janos.illes@formlabs.com>
2024-07-26 16:56:25 +0200

    add errorbox and fix vertical scrollbar appearing

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,17 @@ setup3DCanvas(canvasContainer);
 const text1element = document.getElementById("text-1") as HTMLInputElement;
 const text2element = document.getElementById("text-2") as HTMLInputElement;
 const baseToggle = document.getElementById("base-toggle") as HTMLInputElement;
-
+const errorBox = document.getElementById("errorBox") as HTMLDivElement;
 
 function textsChanged() {
   const text1 = text1element.value.toUpperCase();
   const text2 = text2element.value.toUpperCase();
   if (text1.length != text2.length) {
     console.log("length mismatch");
+    errorBox.style.display = "block";
     return;
+  } else {
+    errorBox.style.display = "none";
   }
   const pairs = [...text2].map((ch, i) => `${ch}${text1[i]}`);
   update3DText(pairs, baseToggle.checked);
